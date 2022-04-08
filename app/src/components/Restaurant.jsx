a80198374d947d181a57f6b5070a3a12048ff193
Shubham Kumar <shubham35godda@gmail.com>
2022-04-09 03:11:05 +0530

    sorted according to the rating

--- app/src/components/Restaurant.jsx
+++ app/src/components/Restaurant.jsx
@@ -15,25 +15,32 @@ const Restaurant = () => {
       console.log(updatedList)
     }
 
-    const lowToHigh=()=>{
-      Menu.price.sort((a,b)=>(a.price-b.price));
-       setMenuData(Menu);
-    }
+    const starItem=(rating)=>{
+      const starList= Menu.filter((currElem)=>{
+         return currElem.rating===rating;
+       })
+       setMenuData(starList);
+      
+     }
+    // const lowToHigh=()=>{
+    //   Menu.price.sort((a,b)=>(a.price-b.price));
+    //    setMenuData(Menu);
+    // }
     
   
   return (
 
     <>
     <div >
-    <button >1Star</button>
-    <button >2Star</button>
-    <button >3Star</button>
-    <button >4Star</button>
+    <button onClick={()=>starItem(1)} >1Star</button>
+    <button onClick={()=>starItem(2)}>2Star</button>
+    <button onClick={()=>starItem(3)}>3Star</button>
+    <button onClick={()=>starItem(4)} >4Star</button>
     <button >Cash</button>
     <button >Card</button>
     <button >All</button>
     <button >HiToLow</button>
-    <button onClick={lowToHigh}>LowToHigh</button>
+    <button>LowToHigh</button>
     <button >Filter&Sort</button>
     <button >4StarAboveinDes</button>
     <button onClick={()=>filterItem("breakfast")} >Breakfast</button>
